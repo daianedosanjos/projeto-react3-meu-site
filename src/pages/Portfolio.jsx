b1f381f61c2fa735d5b1529c3f48511425db9fc3
daiane-dosanjos <dayanjos01@gmail.com>
2022-11-25 16:50:15 -0300

    mudando ícone

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,22 +1,20 @@
-import { Header } from '../components/Header'
-import portfolioImg from '../assets/portfolio.svg'
-import styles from '../styles/pages/portfolio.module.css'
-import { FaGithub } from 'react-icons/Fa'
+import { Header } from "../components/Header";
+import portfolioImg from "../assets/portfolio.svg";
+import styles from "../styles/pages/portfolio.module.css";
+import { FaGithub } from "react-icons/Fa";
 
-
-export function Portfolio () {
+export function Portfolio() {
   return (
     <>
       <Header title="Meus projetinhos" image={portfolioImg} />
-	  <section className={styles.portfolioContainer}>
-		<h2 className={styles.portfolioText}> Veja aqui meu gitHub</h2>   
-	   <a className={styles.portfolioContainer} href="https://github.com/daianedosanjos" target="_blank">
-        <FaGithub className={styles.portfolioLink} />
-      </a>
+      <section className={styles.portfolioContainer}>
+        <h2 className={styles.portfolioText}> Veja aqui meu gitHub</h2>
+        <a className={styles.portfolioContainer}
+          href="https://github.com/daianedosanjos"
+          target="_blank">
+          <FaGithub className={styles.portfolioLink} />
+        </a>
       </section>
-	
     </>
-
-
-  )
+  );
 }
